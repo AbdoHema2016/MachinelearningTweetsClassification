7a74db3b6342f7d058f398b2441993ce20b7c4d9
Abdelrahman-Arw <abdelrahman.arwish@linkdev.com>
2019-12-25 10:58:53 +0200

    Initial Commit

---
+++ MLStockPrediction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MLStockPrediction
+//
+//  Created by Abdelrahman-Arw on 12/25/19.
+//  Copyright © 2019 Abdelrahman-Arw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
